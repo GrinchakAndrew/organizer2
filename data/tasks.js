cc3528f4778607736594d6bbd8ed45017370a990
GrinchakAndrew <andrewgrinchak@gmail.com>
2016-12-05 01:08:36 +0200

    new

---
+++ data/tasks.js
@@ -0,0 +1,154 @@
+{
+	"GTS Induction Basics" : [
+		{"JS Dev Competency Model" : [ 
+					{"Induction Specific Weight" : {"(5) JS RnD Competencies" : " data/InductionJavaScriptRnDCompetencies.txt", "(4) JS Core Knowledge Evaluation" : " data/Induction_JSCoreKnowledgeCompetencires.txt", "(3) JS Core Competencies" : " data/InductionJavaScriptCoreCompetencies.txt", "(2) JS Functional Competencies": " data/Induction_JSFunctionalCompetencies.txt", "(1) Competency Blocks" : "data/Induction_JSCompetencies_blocks.txt"}}
+				  ]
+		},
+		
+		{"Day 1" : [
+					{"OpenAir Helper" : {"Induction: OpenAir Guideline": "data/InductionOAHelper.txt"}},
+					{"YouTrack Helper" : {"(2) YouTrack : Teamwork Communication" : "data/Induction_YT_helper_comments.txt", "(1) YouTrack : General Details": "data/Induction_YouTrackHelper1.txt"}},
+					{"Necessary Toolbox" : {"(5) System Admin Requests" : "data/InductionMISRequests.txt", "(4) 'mmtools' Bookmark" : "data/InductionMmtools.txt", "(3) WebDAV Purpose Description" : " data/InductionWebDAVPurposeDescription.txt", "(2) WebDrive Preset Registry file" : " data/InductionWebDrivePresetRegistryFile.txt", "(1) Induction: Toolbox for Front-end JavaScript Dev": " data/InductionToolBoxForJSDevs.txt"}},
+			        {"BitBucket Helper for Newcomers" : {"BitBucket Helper for Newcomers": " data/InductionGitLabHelper.txt"}},
+				    {"Akamai CDN Drive Mapping" : {"Akamai CDN Drive Mapping": "data/InductionAkamaiCDNDriveMapping.txt"}}
+				  ]
+		}
+	],
+
+	"Induction: Advanced level" : [
+	
+		{"Day 10 - 11 " : [
+					
+					{"Reports" : [
+													{"Campaign Reports" : 
+														{"MaxTest Reports" : "", 
+														  "Scheduled Reports" : "", 
+														  "Report Builder" : ""
+														 }
+													}, 
+													{"System Reports" : 
+														{"Data Changes Audit" : "", 
+														  "Action Log, Action Tracking" : "", 
+														  "Campaign Optimization Method" : "", 
+														  "MaxRecommend Log" : "",
+														  "Product Purchases Log Details" : "", 
+														  "Product Recommendations Log Details" : "", 
+														  "Campaign Status Report" : ""
+														 }
+													}
+												]
+					}
+					
+				  ]
+	     }
+	
+	],
+	"Induction: Final Questionnaire" : [
+				{"Questions" : [
+						{"Account Administration" : {"Account Administration: Questions" : "data/Induction_Questionnaire_Overall.txt"}},
+						{"UI Knowledge" : {
+								"UI Knowledge: Site Level" : "data/Induction_UI_Knowldge_Final_Questions.txt",
+								"UI Knowledge: Campaign Level" : "data/Induction_UI_Knowldge_Final_Questions_Campaign_Level.txt"
+								}
+						},
+						{"Personalisation Criteria" : {"Personalisation Criteria Knowledge: Questions" : "data/Induction_Questionnaire_Overall_PCs.txt"}}, 
+						{"MaxRecommend and Data Feeds" : {
+								"Recommendation Campaigns: Questions" : "data/Induction_Questionnaire_Overall_Recommend.txt",
+								"Data Feeds: Questions" : "data/Induction_Questionnaire_Overall_Data_Feeds.txt"
+							}
+						}
+					]
+				}
+	],
+	
+	"Usable Code" : [
+				{"Cross-Domain Solutions"  : {
+												"Flash-Shared-qwertyqwerty" : "data/FlashSharedObjectCode.txt",
+												"Flash-Shared-asdfghasdfgh" : "data/FlashSharedObject.txt",
+												"Flash-Shared-zxcvbnzxcvbn" : "data/FlashSharedObject.txt",
+												"Flash-Shared Object" : "data/FlashSharedObject.txt", 
+												"Maxymiser Flash" : "data/FlashSharedObjectCampaign.txt"},
+				
+					"D. Podgorniy Javascript-Toolbox" : {"After.js" : "data/After.txt", 
+														"Animate.js" : "data/Animate.txt",
+														"AttachStyle.js" : "data/AttachStyle_Podgorniy.txt",
+														"Bind.js" : "data/Bind_Podgorniy.txt",
+														"Chaining.js" : "data/Chaining_Podgorniy.txt",
+														"Compose.js" : "data/Compose_Podgorniy.txt",
+														"Custom Events.js" : "data/CustomEvents_Podgorniy.txt",
+														"Decorate.js" : "data/Decorate_Podgorniy.txt",
+														"Delay.js" : "data/Delay_Podgorniy.txt",
+														"Delegate.js" : "data/Delegate_Podgorniy.txt",
+														"Event Manager.js" : "data/EventManager_Podgorniy.txt",
+														"ExecOutOfDom.js" : "data/ExecOutOfDom.txt",
+														"Filter.js" : "data/Filter_Podgorniy.txt",
+														"Get First Child.js" : "data/GetFirstChild.txt",
+														"Get Siblings.js" : "data/GetSiblings.txt",
+														"Has Class.js" : "data/HasClass.txt",
+														"IndexOf.js" : "data/IndexOf.txt",
+														"IsIterable.js" : "data/isIterable.txt",
+														"Is Empty Object.js" : "data/isEmpty_Object.txt",
+														"Iterate.js" : "data/Iterate.txt",
+														"IterateAsync.js" : "data/IterateAsync.txt",
+														"Make Iterable.js" : "data/MakeIterable.txt",
+														"Mediator.js" : "data/Mediator.txt",
+														"Next.js" : "data/Next.txt",
+														"Once.js" : "data/Once.txt",
+														"Planned.js" : "data/Planned.txt",
+														"Remove.js" : "data/Remove.txt",
+														"Replace_string_occurances.js" : "data/Replace_string_occurances.txt",
+														"Simulate Mouse Events.js" : "data/SimulateMouseEvents.txt",
+														"Singleton.js" : "data/Singleton.txt",
+														"SmartAsyncIterator.js" : "data/SmartAsyncIterator.txt",
+														"Splitter.js" : "data/Splitter.txt",
+														"Templater.js" : "data/Templater.txt",
+														"ToArray.js" : "data/ToArray.txt",
+														"TopWalker.js" : "data/TopWalker.txt",
+														"Traverse_object.js" : "data/Traverse_object.txt",
+														"CrossBrowserUnbinding.js" : "data/CrossBrowserUnbinding.txt",
+														"Wait.js" : "data/Wait.txt",
+														"WaitFor.js" : "data/WaitFor.txt",
+														"Worker.js" : "data/Worker.txt",
+														"Get-json.js (an Example)" : "data/get-json.js",
+														"Async-reqest.js (an Example)" : "data/async-reqest.js",
+														"Get-xml.js (an Example)" : "data/get-xml.js"
+													},
+					"Cross-Platform Solutions" : {"iPhones Click Events" : "data/TouchClickEventsIos.txt",
+													"Cross-Platform Markup: width" : "data/CrossPlatformMarkupWidth.txt"},
+					"Patterns" : {"Proxy" : "data/Proxy.txt",
+								   "Deferred" : "data/Deferrer.txt", 
+									"Observer" : "data/Observer.txt", 
+									"Event Manager" : "data/EventManager.txt"},
+					"Snippets" : { "AttachStyle": "data/AttachStyle.txt", 
+									"AttachStyle : O. Cherkashchenko": "data/AttachStyleCherkashchenko.txt",
+									"AttachStyle : E. Pavlyuk": "data/AttachStylePavluyk.txt",
+									"Storage" : "data/Storage.txt",
+									"Session Cookie" : "data/SessionCookie.txt",
+									"Checker script by O. Cherkashchenko" : "data/CheckerCherkashchenko.txt",
+									"Action Tracking by A. Tserkovny" : "data/ActionTrackingTserkovny.txt",
+									"Action Tracking by D. Zhuravlev, based on A.Tserkovny": "data/ActionTrackingZhuravlev.txt",
+									"Action Tracking revamp by O. Cherkashchenko" : "data/ActionTrackingCherkashchenko.txt",
+									"Action Tracking simplified by herkashchenko" : "data/ActionTrackingCherkashchenkoSimplified.txt",
+									"Action Tracking without QA by Stanko" : "data/ActionTrackingStankoNoQA.txt",
+									"Action Tracking by James Herrero" : "data/ActionTrackingJamesHerrero.txt",
+									"iFrame Click-through Action Tracking" : "data/iFrameClickThroughTracking.txt",
+									"Campaign Rendering by Cherashchenko" : "data/CampaignRenderingByCherashchenko.txt",
+									"Campaign Initialization by Shveday" : "data/CampaignInitByShveday.txt",
+									"Campaign Initialization by Shveday : description" : "data/CampaignInitByShveday_description.txt",
+									"Mobile Snippet by M. Shtrumin" : "data/MobileByShturmin.txt",
+									"Global PC Snippet" : "data/GlobalPC.txt",
+									"Click-Actions Tracking: Links to Pages Without mmcore": "data/ClickActionOnLinkThatLeadsToAPageWithoutMmcore.txt",
+									"Scroll Distance Action Tracker": "data/ClickActionOnLinkThatLeadsToAPageWithoutMmcore.txt",
+									"Scroll Distance Action Tracker: description" : "data/ScrollDistanceActionTracker_description.txt",
+									"Redirect Snippet": "data/RedirectSnippet.txt",
+									"AJAX Event-Listeners" : "data/ajaxEventListeners.txt", 
+									"One-off CG Requests Persistent PCs, mmcore.js 1.0" : "data/PersistentPCsCode.txt",
+									"Try-Catch Solution" : "data/TryCatchCode.txt"
+							}
+				}], 
+		
+		"Usable Resources" : [{
+			"Best Practices" : {"Usable Links" : "data/Induction_JavaScript_Performance_Best_Practices.txt"}
+		
+		}]				
+}
